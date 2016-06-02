6a4eecaf1211193c6cc1b5c80f3315a117f82c69
Ryan Biwer <ryan@brightcellars.com>
2016-06-02 10:24:13 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function spyback(func, before, after) {
+    return function(...args) {
+        if(before) before(args);
+        let result = func.apply(this, args);
+        if(after) after(args, result);
+    };
+}
+
+module.exports = spyback;
---
+++ test.js
@@ -0,0 +1,46 @@
+"use strict";
+/* jshint mocha: true */
+
+let assert  = require("chai").assert,
+    spyback = require(".");
+
+describe("spyback", function() {
+    it("calls the before function correctly", function(done) {
+        let args = [ "one", 2, [ 3 ] ];
+        let beforeCalledYet = false;
+
+        let before = (actualArgs) => {
+            assert.deepEqual(args, actualArgs);
+            beforeCalledYet = true;
+            done();
+        };
+
+        let myObj = {
+            func() { assert.isOk(beforeCalledYet); }
+        };
+
+        myObj.func = spyback(myObj.func, before);
+        myObj.func(...args);
+    });
+
+    it("calls the after function correctly", function(done) {
+        let args = [ "one", 2, [ 3 ] ];
+        let result = "okay";
+
+        let after = (actualArgs, actualResult) => {
+            assert.deepEqual(args, actualArgs);
+            assert.equal(result, actualResult);
+            done();
+        };
+
+        let myObj = {
+            func(a, b, c) {
+                assert.deepEqual(args, [ a, b, c ]);
+                return result;
+            }
+        };
+
+        myObj.func = spyback(myObj.func, null, after);
+        myObj.func(...args);
+    });
+});
